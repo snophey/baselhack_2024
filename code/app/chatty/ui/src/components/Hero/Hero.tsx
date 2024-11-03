32e4c3976803fdb341c9b926baee37e942c3755b
Alexander Rovner <rovnero@gmail.com>
2024-11-03 08:28:36 +0100

    cosmetic fixes

--- code/app/chatty/ui/src/components/Hero/Hero.tsx
+++ code/app/chatty/ui/src/components/Hero/Hero.tsx
@@ -1,6 +1,13 @@
 import { Center, Flex, Image, Text, Title } from "@mantine/core";
 import logo from "../../assets/navbar.svg";
 
+function G({ children }: { children: any }) {
+  return <span style={{
+    color: '#8ccd10',
+    fontWeight: 750,
+  }}>{children}</span>;
+}
+
 export function Hero() {
   return (
     <Center style={{
@@ -14,7 +21,7 @@ export function Hero() {
         }} />
         <Title order={1}>Hi, ich bin NAVBÄR</Title>
         <Text style={{ textAlign: 'center' }}>
-          Ich habe alle Versicherungsbedingungen gelesen, damit du es nicht tun musst.
+          Ich habe <G>a</G>lle <G>V</G>ersicherungs<G>b</G>edingungen gelesen, damit du es nicht tun musst.
           Lass uns dieses Versicherungsding zusammen verstehen!
         </Text>
       </Flex>
